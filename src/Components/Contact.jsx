651a8416c8a6d4fa4b1945f411f0ae330cc42aa7
BradleyLegge <bmlegge12@gmail.com>
2025-05-17 15:16:10 -0400

    added contact page css

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-router-dom";
 
 const Contact = () => {
@@ -8,19 +8,27 @@ const Contact = () => {
       <Form className="form-container">
         <div className="name-container">
           <div className="first-name-container">
-            <label for="firstName">First Name</label>
+            <label for="firstName">
+              First Name<span className="ast"> *</span>
+            </label>
             <input type="text" id="firstName" name="firstName" />
           </div>
           <div className="last-name-container">
-            <label for="lastname">Last Name</label>
+            <label for="lastname">
+              Last Name<span className="ast"> *</span>
+            </label>
             <input type="text" id="lastname" name="lastname" />
           </div>
         </div>
-        <label for="email">Email Address </label>
+        <label for="email">
+          Email Address<span className="ast"> *</span>
+        </label>
         <input type="text" id="email" name="email" />
         <label for="phoneNumber">Phone Number</label>
         <input type="text" id="phoneNumber" name="phoneNumber"></input>
-        <label for="request">Request</label>
+        <label for="request">
+          Request<span className="ast"> *</span>
+        </label>
         <textarea rows="5" id="request" name="request"></textarea>
         <input type="submit" className="submit-btn"></input>
       </Form>
