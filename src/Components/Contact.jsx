0176f4b2321e2be4de715aad018a93a662c27cc8
Brad <bmlegge12.gmail.com>
2025-05-19 22:16:52 -0400

    Added validation to contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,35 +1,104 @@
 import React, { useState } from "react";
 import { Form } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    reset();
+  };
+
   return (
     <section className="contact-container">
       <h1 className="contact-header">Contact Us</h1>
-      <Form className="form-container">
+      <Form className="form-container" onSubmit={handleSubmit(onSubmit)}>
         <div className="name-container">
           <div className="first-name-container">
-            <label for="firstName">
-              First Name<span className="ast"> *</span>
+            <label htmlFor="firstName" className="first-name">
+              First Name *
+              {errors.firstName && (
+                <p className="input-error">{errors.firstName.message}</p>
+              )}
             </label>
-            <input type="text" id="firstName" name="firstName" />
+            <input
+              className={`${"firstName" in errors && "input-error-border"}`}
+              type="text"
+              id="firstName"
+              {...register("firstName", {
+                required: "First Name required",
+              })}
+            />
           </div>
           <div className="last-name-container">
-            <label for="lastname">
-              Last Name<span className="ast"> *</span>
+            <label htmlFor="lastName" className="last-name">
+              Last Name *
+              {errors.lastName && (
+                <p className="input-error">{errors.lastName.message}</p>
+              )}
             </label>
-            <input type="text" id="lastname" name="lastname" />
+            <input
+              type="text"
+              className={`${"lastName" in errors && "input-error-border"}`}
+              id="lastname"
+              {...register("lastName", {
+                required: "Last Name required",
+              })}
+            />
           </div>
         </div>
-        <label for="email">
-          Email Address<span className="ast"> *</span>
+        <label htmlFor="email" className="email">
+          Email Address *
+          {errors.email && (
+            <p className="input-error">{errors.email.message}</p>
+          )}
         </label>
-        <input type="text" id="email" name="email" />
-        <label for="phoneNumber">Phone Number</label>
-        <input type="text" id="phoneNumber" name="phoneNumber"></input>
-        <label for="request">
-          Request<span className="ast"> *</span>
+        <input
+          type="text"
+          className={`${"email" in errors && "input-error-border"}`}
+          id="email"
+          {...register("email", {
+            required: "E-mail required",
+            pattern: {
+              value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+              message: "Email is not valid",
+            },
+          })}
+        />
+        <label htmlFor="phoneNumber" className="phone-number">
+          Phone Number
+          {errors.phoneNumber && (
+            <p className="input-error">{errors.phoneNumber.message}</p>
+          )}
         </label>
-        <textarea rows="5" id="request" name="request"></textarea>
+        <input
+          type="text"
+          className={`${"phoneNumber" in errors && "input-error-border"}`}
+          id="phoneNumber"
+          {...register("phoneNumber", {
+            pattern: {
+              value: /^\d{3}-\d{3}-\d{4}$/,
+              message: "Phone number is not valid",
+            },
+          })}
+        ></input>
+        <label htmlFor="request" className="request">
+          Request *
+          {errors.request && (
+            <p className="input-error">{errors.request.message}</p>
+          )}
+        </label>
+        <textarea
+          rows="5"
+          className={`${"request" in errors && "input-error-border"}`}
+          id="request"
+          {...register("request", { required: "Request required" })}
+        ></textarea>
         <input type="submit" className="submit-btn"></input>
       </Form>
     </section>
